c74be541e945004ba127578e43c5896f4554e91d
bran <brand@DESKTOP-7A66GEP>
2026-03-29 20:09:53 +0200

    Improved bunnyhop.

--- CSS-MultiHack-Internal/src/bunnyhop.h
+++ CSS-MultiHack-Internal/src/bunnyhop.h
@@ -5,20 +5,30 @@
 
 namespace bunnyhop
 {
-	const uint32_t FLAG_STANDING = 257;
-	const uint32_t FLAG_ONGROUND = 263;
-	const uint32_t JUMP_PRESS = 6;
+    const uint32_t FL_ONGROUND = (1 << 0);
 
-	void Bunnyhop(uintptr_t client, uintptr_t localPlayer)
-	{
-		if (GetAsyncKeyState(VK_SPACE))
-		{
-			uint32_t flag = *reinterpret_cast<std::uint32_t*>(localPlayer + offset::m_fFlags);
+    const uint32_t JUMP_PRESS = 5;
+    const uint32_t JUMP_RELEASE = 4;
 
-			if (flag == FLAG_STANDING || flag == FLAG_ONGROUND)
-			{
-				*reinterpret_cast<uint32_t*>(client + offset::m_dwForceJump) = JUMP_PRESS;
-			}
-		}
-	}
+    void Bunnyhop(uintptr_t client, uintptr_t localPlayer)
+    {
+        if (!(GetAsyncKeyState(VK_SPACE) & 0x8000))
+        {
+            return;
+        }
+
+        uint32_t flag = *reinterpret_cast<std::uint32_t*>(localPlayer + offset::m_fFlags);
+
+        // 2. Use bitwise AND to check for ground contact, ignoring crouch/water flags
+        if (flag & FL_ONGROUND)
+        {
+            // We are on the ground. Send the jump command.
+            *reinterpret_cast<uint32_t*>(client + offset::m_dwForceJump) = JUMP_PRESS;
+        }
+        else
+        {
+            // We are in the air. Force the release state so the game registers the next jump.
+            *reinterpret_cast<uint32_t*>(client + offset::m_dwForceJump) = JUMP_RELEASE;
+        }
+    }
 }
